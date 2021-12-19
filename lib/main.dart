2d305a1aaaf9af212a6e8d90f144b5c107e92603
Yangsongcbu <63138819+Yangsongcbu@users.noreply.github.com>
2021-12-18 17:31:28 -0800

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:weather_app/screens/auth_screen.dart';
+import 'package:weather_app/screens/chat_screen.dart';
+import 'package:weather_app/screens/splash_screen.dart';
+
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+    return FutureBuilder(
+        // Initialize FlutterFire:
+        future: _initialization,
+        builder: (context, appSnapshot) {
+          return MaterialApp(
+            title: 'Weather',
+            theme: ThemeData(
+              backgroundColor: Colors.pink,
+              buttonTheme: ButtonTheme.of(context).copyWith(
+                buttonColor: Colors.pink,
+                textTheme: ButtonTextTheme.primary,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.pink)
+                  .copyWith(secondary: Colors.deepPurple),
+            ),
+            /*home: StreamBuilder(stream: FirebaseAuth.instance.onAuthStateChanged, */
+            home: appSnapshot.connectionState != ConnectionState.done
+                ? SplashScreen()
+                : StreamBuilder(
+                    stream: FirebaseAuth.instance.authStateChanges(),
+                    builder: (ctx, userSnapshot) {
+                      if (userSnapshot.connectionState ==
+                          ConnectionState.waiting) {
+                        return SplashScreen();
+                      }
+                      if (userSnapshot.hasData) {
+                        return ChatScreen();
+                      }
+                      return AuthScreen();
+                    }),
+          );
+        });
+  }
+}
---
+++ lib/screens/chat_screen.dart
@@ -0,0 +1,171 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+import 'package:weather_app/widgets/chat/messages.dart';
+import 'package:weather_app/widgets/chat/new_message.dart';
+
+import 'package:weather_app/data/data_service.dart';
+import 'models.dart';
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  final _cityTextController = TextEditingController();
+  final _dataService = DataService();
+
+  WeatherResponse _response;
+  /*void initState() {
+    super.initState();
+    final fbm = FirebaseMessaging();
+    fbm.requestNotificationPermissions();
+    fbm.configure(onMessage: (msg) {
+      print(msg);
+      return;
+    }, onLaunch: (msg) {
+      print(msg);
+      return;
+    }, onResume: (msg) {
+      print(msg);
+      return;
+    });
+    fbm.subscribeToTopic('chat');
+  }*/
+
+  /*void initState() {
+    super.initState();
+    final fbm = FirebaseMessaging.instance;
+    fbm.requestPermission();
+    FirebaseMessaging.onMessage.listen((message) {
+      print(message);
+      return;
+    });
+    FirebaseMessaging.onMessageOpenedApp.listen((message) {
+      print(message);
+      return;
+    });
+    fbm.subscribeToTopic('chat');
+  }*/
+
+  /*Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('FlutterChat'),
+        actions: [
+          DropdownButton(
+            underline: Container(),
+            icon: Icon(
+              Icons.more_vert,
+              color: Theme.of(context).primaryIconTheme.color,
+            ),
+            items: [
+              DropdownMenuItem(
+                child: Container(
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.exit_to_app),
+                      SizedBox(width: 8),
+                      Text('Logout'),
+                    ],
+                  ),
+                ),
+                value: 'logout',
+              ),
+            ],
+            onChanged: (itemIdentifier) {
+              if (itemIdentifier == 'logout') {
+                FirebaseAuth.instance.signOut();
+              }
+            },
+          ),
+        ],
+      ),
+      body: Container(
+        child: Column(
+          children: <Widget>[
+            Expanded(
+              child: Messages(),
+            ),
+            NewMessage(),
+          ],
+        ),
+      ),
+    );
+  }*/
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        title: Text('Weather Quiry'),
+        actions: [
+          DropdownButton(
+            underline: Container(),
+            icon: Icon(
+              Icons.more_vert,
+              color: Theme.of(context).primaryIconTheme.color,
+            ),
+            items: [
+              DropdownMenuItem(
+                child: Container(
+                  child: Row(
+                    children: <Widget>[
+                      Icon(Icons.exit_to_app),
+                      SizedBox(width: 8),
+                      Text('Logout'),
+                    ],
+                  ),
+                ),
+                value: 'logout',
+              ),
+            ],
+            onChanged: (itemIdentifier) {
+              if (itemIdentifier == 'logout') {
+                FirebaseAuth.instance.signOut();
+              }
+            },
+          ),
+        ],
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            if (_response != null)
+              Column(
+                children: [
+                  Image.network(_response.iconUrl),
+                  Text(
+                    '${_response.tempInfo.temperature}°',
+                    style: TextStyle(fontSize: 40),
+                  ),
+                  Text(_response.weatherInfo.description)
+                ],
+              ),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 50),
+              child: SizedBox(
+                width: 150,
+                child: TextField(
+                    controller: _cityTextController,
+                    decoration: InputDecoration(
+                        labelText: 'Please input city name or zipcode:'),
+                    textAlign: TextAlign.center),
+              ),
+            ),
+            ElevatedButton(onPressed: _search, child: Text('Search'))
+          ],
+        ),
+      ),
+    ));
+  }
+
+  void _search() async {
+    final response = await _dataService.getWeather(_cityTextController.text);
+    setState(() => _response = response);
+  }
+}
